9b425f973a25847e25723a6266a72a9103c9f62c
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-17 14:06:19 +0700

    module2-task1 | Изменение передачи пропсов в Container

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -7,7 +7,7 @@ type MainPageProps = {
 
 export default function MainPage({ cards }: MainPageProps): JSX.Element {
   return (
-    <Container extraClass={'page--gray page--main'} mainClass={'page__main--index'}>
+    <Container extraClass="page--gray page--main" mainClass="page__main--index">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
