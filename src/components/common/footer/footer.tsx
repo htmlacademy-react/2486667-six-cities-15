006d3d02ec4334ec2376743346f15795e212c223
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-20 11:56:58 +0700

    module3-task1 | fix: вывод классов при помощи clsx в Footer

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,10 +1,12 @@
+import {clsx} from "clsx";
+
 type FooterProps = {
-  footerClass?: string;
+  extraClass?: string;
 }
 
-export default function Footer({ footerClass }: FooterProps): JSX.Element {
+export default function Footer({ extraClass }: FooterProps): JSX.Element {
   return (
-    <footer className={`footer ${footerClass ? footerClass : ''}`}>
+    <footer className={clsx('footer', extraClass !== undefined && extraClass)}>
       <a className="footer__logo-link" href="main.html">
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
       </a>
