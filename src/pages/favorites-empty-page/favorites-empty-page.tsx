362c6f2d2bd061c29d6a5a818d9db36eb15cbf93
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-21 12:22:18 +0700

    module3-task1 | Правка компонентов страниц

--- src/pages/favorites-empty-page/favorites-empty-page.tsx
+++ src/pages/favorites-empty-page/favorites-empty-page.tsx
@@ -1,17 +1,24 @@
 import Container from '../../components/common/container/container';
+import ExtraContainer from '../../components/common/extra-container/extra-container';
+import Header from '../../components/common/header/header';
+import Footer from '../../components/common/footer/footer';
 
 export default function FavoritesEmptyPage(): JSX.Element {
   return (
-    <Container extraClass="page__main--favorites page__main--favorites-empty">
-      <div className="page__favorites-container container">
-        <section className="favorites favorites--empty">
-          <h1 className="visually-hidden">Favorites (empty)</h1>
-          <div className="favorites__status-wrapper">
-            <b className="favorites__status">Nothing yet saved.</b>
-            <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
-          </div>
-        </section>
-      </div>
-    </Container>
+    <ExtraContainer extraClass="page--favorites-empty">
+      <Header />
+      <Container extraClass="page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+            </div>
+          </section>
+        </div>
+      </Container>
+      <Footer />
+    </ExtraContainer>
   );
 }
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,8 +1,15 @@
+import ExtraContainer from '../../components/common/extra-container/extra-container';
+import Container from '../../components/common/container/container';
+import Header from '../../components/common/header/header';
+
 export default function NotFoundPage(): JSX.Element {
   return (
-    <div>
-      <div>Страница не найдена</div>
-      <a href="/">На главную</a>
-    </div>
+    <ExtraContainer>
+      <Header />
+      <Container extraClass="page--not-found">
+        <div>Страница не найдена</div>
+        <a href="/">На главную</a>
+      </Container>
+    </ExtraContainer>
   );
 }
