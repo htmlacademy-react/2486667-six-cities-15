9b1a5b3a1ae699aba8fae275b17cac0f100261f1
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-21 12:25:44 +0700

    module3-task1 | Добавлены маршруты еще для 3х страниц

--- src/components/common/app/app.tsx
+++ src/components/common/app/app.tsx
@@ -1,5 +1,8 @@
 import {Route, Routes} from 'react-router-dom';
 import MainPage from '../../../pages/main-page/main-page';
+import LoginPage from '../../../pages/login-page/login-page';
+import FavoritesPage from '../../../pages/favorites-page/favorites-page';
+import OfferPage from '../../../pages/offer-page/offer-page';
 
 type AppProps = {
   cards: string[];
@@ -10,7 +13,19 @@ export default function App({ cards }: AppProps): JSX.Element {
     <Routes>
       <Route
         path="/"
-        element={<MainPage cards={cards}/>}
+        element={<MainPage cards={cards} />}
+      />
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+      <Route
+        path="/favorites"
+        element={<FavoritesPage />}
+      />
+      <Route
+        path="/offer/:id"
+        element={<OfferPage />}
       />
     </Routes>
   );
