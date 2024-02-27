c0888338641777d9f42e4d92db6c5177c815316d
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-27 15:58:35 +0700

    module4-task1 | Вернула защищенный маршрут FavoritePage

--- src/components/common/app/app.tsx
+++ src/components/common/app/app.tsx
@@ -3,9 +3,10 @@ import MainPage from '../../../pages/main-page/main-page';
 import LoginPage from '../../../pages/login-page/login-page';
 import FavoritesPage from '../../../pages/favorites-page/favorites-page';
 import NotFoundPage from '../../../pages/not-found-page/not-found-page';
-import {AppRoute} from '../../../const';
+import {AppRoute, AuthStatus} from '../../../const';
 import OfferPage from '../../../pages/offer-page/offer-page';
 import {Offer} from '../../../types/offer';
+import ProtectedRoute from '../protected-route/protected-route';
 
 type AppProps = {
   offers: Offer[];
@@ -24,8 +25,7 @@ export default function App({ offers }: AppProps): JSX.Element {
       />
       <Route
         path={AppRoute.Favorites}
-        //element={<ProtectedRoute authStatus={AuthStatus.NoAuth}><FavoritesPage /></ProtectedRoute>}
-        element={<FavoritesPage offers={offers} />}
+        element={<ProtectedRoute authStatus={AuthStatus.NoAuth}><FavoritesPage offers={offers} /></ProtectedRoute>}
       />
       <Route
         path={`${AppRoute.Offer}/:id`}
