d6a871691fca490a8274758f9f0f9adb568004c9
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-28 21:53:34 +0700

    module4-task1 | Правка 404 Page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -4,34 +4,35 @@ import Footer from '../../components/common/footer/footer';
 import {Link, useLocation} from 'react-router-dom';
 import {AppRoute} from '../../const';
 import MainContainer from '../../components/common/main-container/main-container';
+import {Helmet} from 'react-helmet-async';
 
 export default function NotFoundPage(): JSX.Element {
   const location = useLocation();
 
   return (
-    <Container extraClass="page--gray page--login">
+    <Container extraClass="page--gray page--main">
+      <Helmet>
+        <title>6 cities: error 404 page</title>
+      </Helmet>
+
       <Header />
-      <MainContainer extraClass="page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">404. Страница не найдена</h1>
-            <p style={{position: 'relative', zIndex: '1', wordWrap: 'break-word'}}>
-              Страница по адресу <b>{location.pathname}</b> не найдена.
-            </p>
-            <br/>
-            <Link to={AppRoute.Root} style={{color: '#4481c3'}}>
-              Перейти на главную
-            </Link>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <Link className="locations__item-link" to="#">
-                <span>Amsterdam</span>
-              </Link>
-            </div>
-          </section>
+
+      <MainContainer extraClass="page__main--index">
+        <div className="container">
+          <h1 className="login__title">404. Страница не найдена</h1>
+
+          <p style={{position: 'relative', zIndex: '1', wordWrap: 'break-word'}}>
+            Страница по адресу <b>{location.pathname}</b> не найдена.
+          </p>
+
+          <br/>
+
+          <Link to={AppRoute.Root} style={{color: '#4481c3'}}>
+            Перейти на главную
+          </Link>
         </div>
       </MainContainer>
+
       <Footer />
     </Container>
   );
