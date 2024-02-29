5c4bece3bb317ceb250ab1a7c6e18fb4899f3346
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-02-29 20:46:05 +0700

    module4-task1 | fix build bugs 9

--- src/components/common/protected-route/protected-route.tsx
+++ src/components/common/protected-route/protected-route.tsx
@@ -1,7 +1,7 @@
 import {Navigate, useLocation} from 'react-router-dom';
 import {AppRoute, AuthStatus} from '../../../const';
 import {setAuthStatus} from '../../../utils/common';
-import {Location} from '@remix-run/router/history';
+//import {Location} from '@remix-run/router/history';
 
 type ProtectedRouteProps = {
   onlyUnAuth?: boolean;
@@ -13,18 +13,19 @@ type FromState = {
 }
 
 export default function ProtectedRoute({ onlyUnAuth, children }: ProtectedRouteProps): JSX.Element {
-  const location = useLocation() as Location<FromState>;
+  //const location = useLocation() as Location<FromState>;
   const isAuthenticate = setAuthStatus(AuthStatus.Auth);
 
   // Авторизованы и стр логина => переход на предыдущую перед стр логина страницу
   if (isAuthenticate && onlyUnAuth) {
-    const from = location.state?.from || { pathname: AppRoute.Root };
-    return <Navigate to={from} />;
+    //const from = location.state?.from || { pathname: AppRoute.Root };
+    //return <Navigate to={from} />;
+    return <Navigate to={AppRoute.Root} />;
   }
 
   // Не авторизованы и не стр логина => переход на стр логина
   if (!isAuthenticate && !onlyUnAuth) {
-    return <Navigate to={AppRoute.Login} state={{from: location}} />;
+    return <Navigate to={AppRoute.Login} /*state={{from: location}}*/ />;
   }
 
   return children;
