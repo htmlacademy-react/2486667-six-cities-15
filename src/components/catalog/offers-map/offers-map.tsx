9dd4c51075128270532f064c67d8a2c3127e4dc1
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-01 21:40:45 +0700

    module5-task1 | создан компонент MapLeaflet и импортирован leaflet

--- src/components/catalog/offers-map/offers-map.tsx
+++ src/components/catalog/offers-map/offers-map.tsx
@@ -1,4 +1,5 @@
 import {Offer} from '../../../types/offer';
+import MapLeaflet from '../../common/map-leaflet/map-leaflet';
 
 type MapProps = {
   offers: Offer[];
@@ -7,10 +8,11 @@ type MapProps = {
 export default function OffersMap({ offers }: MapProps) {
   return (
     <>
-      {/*<section className="cities__map map"></section>*/}
+      {/*<section className="cities__map map-leaflet"></section>*/}
       {offers && offers.map((offer) => (
         <div key={offer.id}>{offer.city.name} - {offer.city.location.latitude}, {offer.city.location.longitude}</div>
       ))}
+      <MapLeaflet />
     </>
   );
 }
---
+++ src/components/common/map-leaflet/map-leaflet.tsx
@@ -0,0 +1,10 @@
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default function MapLeaflet() {
+  return (
+    <div style={{height: '400px'}}>
+
+    </div>
+  );
+}
