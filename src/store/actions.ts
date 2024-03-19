c1bf210df281543e8689aaca0baeefc1c1b4f3f1
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-19 12:42:32 +0700

    module6-task1 | изменен синтаксис передачи payload в actions

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,14 +1,11 @@
 import {createAction} from '@reduxjs/toolkit';
 import {City} from '@/types/city';
+import {Offer} from '@/types/offer';
 
-export const fillingOffers = createAction('offers/fillingOffers');
+export const fillingOffers = createAction<Array<Offer>>('offers/fillingOffers');
 
-export const changeCity = createAction('cities/changeCity', (city: City) => ({
-  payload: city,
-}));
+export const changeCity = createAction<{city: City}>('cities/changeCity');
 
-export const setSortId = createAction('offers/setSortId', (id: number) => ({
-  payload: id,
-}));
+export const setSortId = createAction<{id: number}>('offers/setSortId');
 
 export const sortOffers = createAction('offers/sort');
