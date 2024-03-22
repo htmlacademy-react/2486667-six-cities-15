d9f0cd118232b844ca2b1721cef9621832ef4054
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-22 17:09:27 +0700

    module7-task1 | Подготовлен модуль для работы с LocalStorage

---
+++ src/services/token.ts
@@ -0,0 +1,17 @@
+const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
+
+type Token = string;
+
+export const getToken = (): Token => {
+  const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
+
+  return token;
+}
+
+export const setToken = (token: Token) => {
+  localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+}
+
+export const removeToken = () => {
+  localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+}
