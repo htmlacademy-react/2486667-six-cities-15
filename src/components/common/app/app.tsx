15c7a2f6a1543ef67f4772b12c9f4ff3074cea09
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-23 12:33:16 +0700

    module7-task1 | Добавлена обработка ошибок взаимодействия с сервером

--- src/components/common/app/app.tsx
+++ src/components/common/app/app.tsx
@@ -10,7 +10,7 @@ import ProtectedRoute from '@/components/common/protected-route/protected-route'
 import {Review} from '@/types/reviews';
 import {CITIES} from '@/mocks/cities';
 import {useAppDispatch} from '@/hooks/store/store';
-import {fetchOffersAction} from '@/store/api-actions';
+import {checkAuthAction, fetchOffersAction} from '@/store/api-actions';
 
 type AppProps = {
   cities: City[];
@@ -20,6 +20,7 @@ type AppProps = {
 export default function App({ cities, reviews }: AppProps): JSX.Element {
   const dispatch = useAppDispatch();
   dispatch(fetchOffersAction());
+  dispatch(checkAuthAction());
 
   return (
     <Routes>
