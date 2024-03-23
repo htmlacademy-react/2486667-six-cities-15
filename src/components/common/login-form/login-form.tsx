5f51e9c7f864f57d42f5e2d2ce778f1dcc54ccc0
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-23 13:28:35 +0700

    module7-task1 | Обновлен компонент LoginForm

--- src/components/common/login-form/login-form.tsx
+++ src/components/common/login-form/login-form.tsx
@@ -1,35 +1,41 @@
-import {ReactEventHandler, useState} from 'react';
-
-type TDataForm = {
-  email: string;
-  password: string;
-}
-
-type TFieldChangeHandler = ReactEventHandler<HTMLInputElement>;
+import {FormEvent, useRef} from 'react';
+import {useAppDispatch} from '@/hooks/store/store';
+import {loginAction} from '@/store/api-actions';
+import {AuthData} from '@/types/user';
 
 export default function LoginForm() {
-  const [dataForm, setDataForm] = useState<TDataForm>({
-    email: '',
-    password: '',
-  });
+  const dispatch = useAppDispatch();
+  const login = useRef<HTMLInputElement | null>(null);
+  const password = useRef<HTMLInputElement | null>(null);
 
-  const fieldChangeHandler: TFieldChangeHandler = (event) => {
-    const { name, value } = event.currentTarget;
-    setDataForm({...dataForm, [name]: value});
+  const formSubmitHandle = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (login.current !== null && password.current !== null) {
+      const authData: AuthData = {
+        login: login.current?.value || '',
+        password: password.current?.value || '',
+      };
+      dispatch(loginAction(authData));
+    }
   };
 
   return (
-    <form className="login__form form" action="#" method="post">
+    <form className="login__form form"
+      action=""
+      method="post"
+      onSubmit={formSubmitHandle}
+    >
       <div className="login__input-wrapper form__input-wrapper">
         <label className="visually-hidden">E-mail</label>
         <input
           className="login__input form__input"
           type="email"
-          name="email"
+          name="login"
           placeholder="Email"
           required
-          value={dataForm.email}
-          onChange={fieldChangeHandler}
+          value="Oliver.conner@gmail.com"
+          ref={login}
         />
       </div>
       <div className="login__input-wrapper form__input-wrapper">
@@ -41,8 +47,8 @@ export default function LoginForm() {
           placeholder="Password"
           required
           autoComplete="on"
-          value={dataForm.password}
-          onChange={fieldChangeHandler}
+          value="password1"
+          ref={password}
         />
       </div>
       <button className="login__submit form__submit button" type="submit">Sign in</button>
