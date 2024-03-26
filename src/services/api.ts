d29aedcdda6c197b17a358d68860759a7c3ccbf1
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-26 22:56:25 +0700

    module7-task2 | Проверка структуры ответа об ошибке от серевера

--- src/services/api.ts
+++ src/services/api.ts
@@ -42,7 +42,9 @@ export const createAPI = (): AxiosInstance => {
       if (error.response && shouldDisplayError(error.response)) {
         const detailMessage = (error.response.data);
 
-        toast.warn(detailMessage.message);
+        if (detailMessage && detailMessage.hasOwnProperty('message')) {
+          toast.warn(detailMessage.message);
+        }
       }
 
       throw error;
