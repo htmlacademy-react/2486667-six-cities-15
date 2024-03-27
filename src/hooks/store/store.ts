276ef52457a37d37d3e5757d1889719c3d30e55d
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-27 20:24:29 +0700

    module7-task2 | Исключение для линтера

--- src/hooks/store/store.ts
+++ src/hooks/store/store.ts
@@ -13,6 +13,7 @@ const useActionCreators = <Actions extends ActionCreatorsMapObject>(actions: Act
   // eslint-disable-next-line react-hooks/exhaustive-deps
   return useMemo(() => bindActionCreators(actions, dispatch), []);
 };
+/* eslint-disable @typescript-eslint/no-explicit-any */
 type BoundActions<Actions extends ActionCreatorsMapObject> = {
   [key in keyof Actions]: Actions[key] extends AsyncThunk<any, any, any> ? BoundAsyncThunk<Actions[key]> : Actions[key];
 };
